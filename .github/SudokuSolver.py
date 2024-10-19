64db426c8d49b8d37ea014c08106d889eaa41ed1
Brandon Vo <vobrandon89@gmail.com>
2024-10-18 18:19:19 -0700

    Completed Sudoku Solver

--- .github/SudokuSolver.py
+++ .github/SudokuSolver.py
@@ -30,34 +30,34 @@ class Sudoku:
             if column == board_size: # Once column is filled, go to next row
                 return solve(row+1, 0)
             
-            if board[row][column] == ".": # If space isn't occupied, iterate through algorithm
+            if board[row][column] == 0: # If space isn't occupied, iterate through algorithm
                 for i in range(1, board_size+1):
-                    if empty_space(row, column, str(i)): # If conditions are met, add 'i' to board
-                        board[row][column] = str(i) 
+                    if empty_space(row, column, i): # If conditions are met, add 'i' to board
+                        board[row][column] = i 
                         
                         if solve(row, column + 1): # Check if the algorithm can fill the next column
                             return True
                         else: 
-                            board[row][column] = "." # Backtrack by resetting the space as empty and use the next index
+                            board[row][column] = 0 # Backtrack by resetting the space as empty and use the next index
                 return False # Return false if another number is found on the conditional (row, col, 3x3) 
             else:
                 return solve(row, column + 1) # Move to next column
-    
+            
         # Once all rows and columns are filled, print out solved sudoku board
         if solve(0,0): 
             for row in board:
                 print(row)
 
 # Example board in LeetCode
-sudokuBoard = [["5","3",".",".","7",".",".",".","."],
-               ["6",".",".","1","9","5",".",".","."],
-               [".","9","8",".",".",".",".","6","."],
-               ["8",".",".",".","6",".",".",".","3"],
-               ["4",".",".","8",".","3",".",".","1"],
-               ["7",".",".",".","2",".",".",".","6"],
-               [".","6",".",".",".",".","2","8","."],
-               [".",".",".","4","1","9",".",".","5"],
-               [".",".",".",".","8",".",".","7","9"]]
+sudoku_board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+                [6, 0, 0, 1, 9, 5, 0, 0, 0],
+                [0, 9, 8, 0, 0, 0, 0, 6, 0],
+                [8, 0, 0, 0, 6, 0, 0, 0, 3],
+                [4, 0, 0, 8, 0, 3, 0, 0, 1],
+                [7, 0, 0, 0, 2, 0, 0, 0, 6],
+                [0, 6, 0, 0, 0, 0, 2, 8, 0],
+                [0, 0, 0, 4, 1, 9, 0, 0, 5],
+                [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 sudokuTest = Sudoku() 
-sudokuTest.solveSudoku(sudokuBoard) # Method call for example board
+sudokuTest.solveSudoku(sudoku_board) # Method call for example board
 
